621b991e766b2f3110025fca4ed237feae5b33c1
marvin2309 <marfluegge@gmail.com>
2023-01-18 22:29:49 +0100

    Hello World 2

--- src/main/java/com/marvin2309/Main.java
+++ src/main/java/com/marvin2309/Main.java
@@ -3,15 +3,17 @@ package com.marvin2309;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping(value = "/greet")
+    @GetMapping("/greet")
     public String greet() {
-        return "Hello World";
+        return "greet";
     }
 }
